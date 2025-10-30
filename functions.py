c45306accf57d5c0f33b0b130dd4a903ad71f7de
amansatch <126454511+amansatch@users.noreply.github.com>
2025-10-30 17:11:28 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,21 +1,20 @@
 import os
 
-def get_user_filepath(username):
-    """Create a separate file for each user."""
-    folder = "user_data"
-    os.makedirs(folder, exist_ok=True)
-    return os.path.join(folder, f"todos_{username}.txt")
+def get_todos(username, basepath="data"):
+    os.makedirs(basepath, exist_ok=True)
+    filepath = os.path.join(basepath, f"{username}.txt")
 
-def get_todos(username):
-    filepath = get_user_filepath(username)
     if not os.path.exists(filepath):
-        with open(filepath, 'w') as file:
-            pass  # create empty file
+        open(filepath, 'w').close()
+
     with open(filepath, 'r') as file:
         todos_local = file.readlines()
     return todos_local
 
-def write_todos(todos_arg, username):
-    filepath = get_user_filepath(username)
+
+def write_todos(todos_arg, username, basepath="data"):
+    os.makedirs(basepath, exist_ok=True)
+    filepath = os.path.join(basepath, f"{username}.txt")
+
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
