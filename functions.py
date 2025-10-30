6a3c2ec26db4d68a9d998bdd636eaa181e14d842
amansatch <126454511+amansatch@users.noreply.github.com>
2025-10-30 19:32:37 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,13 +1,13 @@
-FILEPATH = "todos.txt"
-
-def get_todos(filepath=FILEPATH):
+def get_todos(username):
+    filepath = f"todos_{username}.txt"
     try:
         with open(filepath, 'r') as file_local:
             todos_local = file_local.readlines()
         return todos_local
     except FileNotFoundError:
-        return []  # Return empty list if file doesn't exist
+        return []
 
-def write_todos(todos_arg, filepath=FILEPATH):
+def write_todos(todos_arg, username):
+    filepath = f"todos_{username}.txt"
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
